cd0b92fa7e2bac75c24397b8967bdd99ce3cd072
jmbalmaceda <jmbalmaceda@gmail.com>
2017-08-09 03:03:44 -0300

    Agregados comentarios a los tests

--- src/test/java/com/cito/sinide/sinideacl/SinideAclApplicationTests.java
+++ src/test/java/com/cito/sinide/sinideacl/SinideAclApplicationTests.java
@@ -26,12 +26,24 @@ public class SinideAclApplicationTests {
 		
 		// Cargo las pruebas
 		AclAccion accion = null;
+		/*
+		 * usuario 1 puede VER juridiscción 13
+		 */
 		accion = crearAccion("VER", 1L, 13L, null, null, null, null);
 		accionRepository.save(accion);
+		/*
+		 * usuario 1 puede CREAR_TITULACION en la jurisdiccion 13
+		 */
 		accion = crearAccion("CREAR_TITULACION", 1L, 13L, null, null, null, null);
 		accionRepository.save(accion);
-		accion = crearAccion("TOMAR_TITULACION", 1L, 14L, 2L, 111L, null, null);
+		/*
+		 * usuario 1 puede TOMAR_TITULACION en la jurisdiccion 14 nivelSevicio 2
+		 */
+		accion = crearAccion("TOMAR_TITULACION", 1L, 14L, 2L, null, null, null);
 		accionRepository.save(accion);
+		/*
+		 * usuario 2 puede TOMAT_ASISTENCIA en la jurisdiccion 14
+		 */
 		accion = crearAccion("TOMAR_ASISTENCIA", 2L, 14L, null, null, null, null);
 		accionRepository.save(accion);
 	}
@@ -66,24 +78,55 @@ public class SinideAclApplicationTests {
 	@Test
 	public void testPuede(){
 		Hashtable<String, Long> info = null;
+		/*
+		 * ¿el usuario 1 puede VER la jurisdiccion 13? Si
+		 */
 		info = crearInfo(13L, null, null, null, null);
 		Assert.assertTrue(accionRepository.puede(1L, "VER", "jurisdiccion", 13L, info));
+		/*
+		 * ¿el usuario 1 puede VER la jurisdicción 14? No
+		 */
 		info = crearInfo(13L, null, null, null, null);
 		Assert.assertFalse(accionRepository.puede(1L, "VER", "jurisdiccion", 14L, info));
+		/*
+		 * ¿el usuario 1 puede CREAR_TITULACION en jurisdiccion 13? Si
+		 */
 		info = crearInfo(13L, null, null, null, null);
 		Assert.assertTrue(accionRepository.puede(1L, "CREAR_TITULACION", "jurisdiccion", 13L, info));
-		info = crearInfo(13L, null, null, null, null);
+		/*
+		 * ¿el usuario 1 puede CREAR_TITULACION en jurisdiccion 14? No
+		 */
+		info = crearInfo(14L, null, null, null, null);
 		Assert.assertFalse(accionRepository.puede(1L, "CREAR_TITULACION", "jurisdiccion", 14L, info));
+		/*
+		 * ¿el usuario 1 puede TOMAR_TITULACION en el nivelServicio 2 de la jurisdiccion 14? Si
+		 */
 		info = crearInfo(14L, 2L, null, null, null);
 		Assert.assertTrue(accionRepository.puede(1L, "TOMAR_TITULACION", "nivelServicio", 2L, info));
+		/*
+		 * ¿el usuario 1 puede TOMAR_TITULACION en el nivelServicio 3 de la jurisdiccion 14? No
+		 */
 		info = crearInfo(14L, 3L, null, null, null);
 		Assert.assertFalse(accionRepository.puede(1L, "TOMAR_TITULACION", "nivelServicio", 3L, info));
+		/*
+		 * ¿el usuario 1 puede TOMAR_TITULACION en el nivelServicio 2 de la jurisdiccion 13? No
+		 */
 		info = crearInfo(13L, 2L, null, null, null);
 		Assert.assertFalse(accionRepository.puede(1L, "TOMAR_TITULACION", "nivelServicio", 2L, info));
-		info = crearInfo(13L, null, null, null, null);
+		/*
+		 * ¿el usuario 1 puede TOMAR_ASISTENCIA en la jurisdiccion 15? No
+		 */
+		info = crearInfo(15L, null, null, null, null);
 		Assert.assertFalse(accionRepository.puede(1L, "TOMAR_ASISTENCIA", "jurisdiccion", 15L, info));
+		/*
+		 * ¿el usuario 2 puede TOMAR_ASISTENCIA en el nivelServicio 99 de la jurisdiccion 14? Si,
+		 * porque puede TOMAR_ASISTENCIA en la jurisdicción 14
+		 */
 		info = crearInfo(14L, 99L, null, null, null);
 		Assert.assertTrue(accionRepository.puede(2L, "TOMAR_ASISTENCIA", "nivelServicio", 99L, info));
+		/*
+		 * ¿el usuario 2 pude TOMAR_ASISTENCIA en el nivelServicio 99 de la jurisdiccion 13? No
+		 */
 		info = crearInfo(13L, 99L, null, null, null);
 		Assert.assertFalse(accionRepository.puede(2L, "TOMAR_ASISTENCIA", "nivelServicio", 99L, info));
 	}
